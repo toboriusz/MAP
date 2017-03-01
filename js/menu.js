96f97e00baaf61a52bea0b999d0db9c4ccc5b3de
Dawid Maroszek <dwdmaroszek@hotmail.com>
2017-03-01 09:58:26 +0100

    menu

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,31 @@
 module.exports = function(){
-	$('.toggle-menu').click(function(){
-		$($(this).attr('data-target')).toggleClass('opened');
+	$(function() {
+
+		var $el, leftPos, newWidth,
+		$mainNav = $("#navigation");
+
+		$mainNav.append("<li id='magic-line'></li>");
+		var $magicLine = $("#magic-line");
+
+		$magicLine
+		.width($(".active").width())
+		.css("left", $(".active a").position().left)
+		.data("origLeft", $magicLine.position().left)
+		.data("origWidth", $magicLine.width());
+
+		$("#navigation li a").hover(function() {
+			$el = $(this);
+			leftPos = $el.position().left;
+			newWidth = $el.parent().width();
+			$magicLine.stop().animate({
+				left: leftPos,
+				width: newWidth
+			});
+		}, function() {
+			$magicLine.stop().animate({
+				left: $magicLine.data("origLeft"),
+				width: $magicLine.data("origWidth")
+			});    
+		});
 	});
 }
